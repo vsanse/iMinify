e4b6a12e4211681eca23087819fcfb7edf59d55b
vsanserwal <vsanserwal@sapient.com>
2019-05-19 18:31:48 +0530

    ioptim done

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# ioptim
-Image optimization npm module
+# iOptim [![Maintainability](https://api.codeclimate.com/v1/badges/ab398b1a9bcb2a2d50a7/maintainability)](https://codeclimate.com/github/vsanse/iOptim/maintainability) [![npm version](http://img.shields.io/npm/v/ioptim.svg?style=flat)](https://npmjs.org/package/ioptim "View this project on npm")
+iOptim is image optimization utility based on [imagemin](https://www.npmjs.com/package/imagemin) package. It currently supports optimization for:
+- png
+- jpg/jpeg
+- svg
+- gif
+
+# Usage
+iOptim is simple cli utility tool.
+```sh
+$ npm install -g iOptim
+$ optimize [options]
+```
+
+## Options
+#### --quality or -q : 
+Define quality of images[1-100].  
+
+Type: `number`  
+image Type: `jpg/jpeg, png`  
+Default: `75`  
+
+#### --progressive, -p : 
+If you want progressive.[true/false]  
+
+Type: `boolean`  
+image Type: `jpg/jpeg`  
+Default: `false`  
+
+#### --speed or -s : 
+Type: `number`  
+Default: `1`  
+image Type: `png`  
+Values: `1 (brute-force) to 11 (fastest)`  
+
+Speed 10 has 5% lower quality, but is about 8 times faster than the default. Speed 11 disables dithering and lowers compression level.
+
+#### --optimizelevel or -ol : 
+Type: `number`   
+Default: `2`  
+Values: `1 to 3`  
+image Type: `gif`  
+
+Select an optimization level between 1 and 3.
+
+The optimization level determines how much optimization is done; higher levels take longer, but may have better results.
+
+1. Stores only the changed portion of each image.  
+2. Also uses transparency to shrink the file further.
+3. Try several optimization methods (usually slower, sometimes better results)  
+
+#### --interlaced or -il : 
+Type: `boolean`  
+Default: `true`  
+Values: `true/false`  
+image Type: `gif`  
+Interlace gif for progressive rendering.  
+
+#### --outputDir or -o  
+Default: `Current Directory`  
+Define output folder name(Default: replace files with optimized ones at same location.  
+
+#### --inputDir or -i  
+Default: `Current Directory`  
+Define images folder name(Default: traverse all sub-directories in current directory).  
+
+### Development
+
+Want to contribute? Great!
+Make a pull request now!
+
+### Todos
+
+ - Add optimization for video files
+
+License
+----
+
+MIT
+
+
+**Free Software, Hell Yeah!**
+ 
---
+++ ioptim.js
@@ -0,0 +1,131 @@
+#!/usr/bin/env node
+const fs = require("fs"),
+      path = require("path"),
+      yargs = require('yargs'),
+      chalk = require("chalk"),
+      figlet = require("figlet"),
+      clear = require("clear"),
+      confirm = require('prompt-confirm'),
+      imgEngines   = require('./engine'), 
+      CURR_DIR = process.cwd(),
+      engines  = new imgEngines(CURR_DIR);
+      
+clear();
+console.log(
+    chalk.bold.yellow(
+        figlet.textSync("iOptim", { horizontalLayout: "full" })
+    )
+);
+
+const argv = yargs
+    .scriptName('optimize')
+    .usage('Usage: $0 [options]')
+    .option('quality', {
+        alias: 'q',
+        description: 'Define quality of images[1-100](Default:75)',
+        type: 'number',
+    })
+    .option('progressive', {
+        alias: 'p',
+        description: 'If want progressive Jpeg (Default:false)',
+        type: 'boolean',
+    })
+    .option('speed', {
+        alias: 's',
+        description: 'Handles dithering and compression level(png)[1-11](Default:1)',
+        type: 'number',
+    })
+    .option('optimizeLevel', {
+        alias: 'ol',
+        description: 'Select an optimization level between 1 and 3[gif].The optimization level determines how much optimization is done; higher levels take longer, but may have better results.(Default:2)',
+        type: 'number',
+    })
+    .option('interlaced', {
+        alias: 'il',
+        description: 'Interlace gif for progressive rendering[gif].(Default:true)',
+        type: 'boolean',
+    })
+    .option('outputDir', {
+        alias: 'o',
+        description: 'Define output folder name(Default: replace files with optimized ones at same location)',
+        type: 'string',
+    })
+    .option('inputDir', {
+        alias: 'i',
+        description: 'Define images folder name(Default: traverse all sub-directories in current directory)',
+        type: 'string',
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const outputDir = argv.outputDir,
+      inputDir = argv.inputDir,
+      inputPath = inputDir?path.join(CURR_DIR, inputDir): CURR_DIR,
+      quality  = argv.quality?argv.quality:75,
+      progressive = argv.progressive?argv.progressive:false
+      speed = argv.speed?argv.speed:1,
+      optimizeLevel = argv.optimizeLevel?argv.optimizeLevel:2,
+      interlaced = argv.interlaced?argv.interlaced:true;
+
+function walk(dir, outputPath){
+    const filesToWalk = fs.readdirSync(dir);
+    // Run optimization engines on directory
+    engines.optimizejpg(dir, outputPath, quality, progressive);
+    engines.optimizepng(dir, outputPath, quality, speed);
+    engines.optimizegif(dir, outputPath, interlaced, optimizeLevel);
+    engines.optimizesvg(dir,outputPath);
+    // get next directory
+    filesToWalk.forEach(file => {
+        const origFilePath = path.join(dir, file);
+        // get stats about the current file
+        const stats = fs.statSync(origFilePath);
+        if (stats.isDirectory()) {
+            if(outputDir){
+                let filePath = path.join(dir, file).split(inputPath)[1];
+                outputPath = path.join(CURR_DIR, outputDir, filePath);
+            }
+            else{
+                outputPath = path.join(dir, file);
+            }
+            // recursive call
+            walk(
+                path.join(dir, file),
+                outputPath
+            );
+        }
+    });
+}
+
+function start(){
+    if(inputDir){
+        walk(
+            inputPath,
+            outputDir?path.join(CURR_DIR, outputDir,inputDir):path.join(CURR_DIR, inputDir)
+        );
+    }
+    else{
+        walk(
+            inputPath,
+            outputDir?path.join(CURR_DIR, outputDir):CURR_DIR
+        );
+    }
+}
+
+if(!outputDir){
+    console.log(chalk.bold.red("This will replace current images with minified/optimized images."));
+    const prompt = new confirm(chalk.red("Continue?"));
+    prompt.originalDefault = false;
+    prompt.run()
+    .then(function(answer) {
+        if(answer){
+            start();
+        }
+        else{
+            yargs.showHelp();
+        }
+    }); 
+}
+else{
+    start();
+}
